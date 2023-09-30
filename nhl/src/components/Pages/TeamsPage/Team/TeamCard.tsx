06a8caf41f0dc76fe4e087723063e3e93dfaa91c
Shama8nchez <shamanouski@gmail.com>
2023-09-30 21:31:27 +0300

    feat: add adaptive for teams page

--- nhl/src/components/Pages/TeamsPage/Team/TeamCard.tsx
+++ nhl/src/components/Pages/TeamsPage/Team/TeamCard.tsx
@@ -16,7 +16,7 @@ function TeamCard(props: {team: Team}) {
   return (
     <div className={classes.team} key={props.team.id}>
       <Logo logo={Logos[props.team.name]} size={'small'}/>
-      <h2 className={classes.teamName}>{props.team.name}</h2>
+      <h2 className={classes.teamName}>{window.screen.width >= 1000 || (window.screen.width < 768 &&  window.screen.width > 485) ? props.team.name : props.team.abbreviation}</h2>
       <div className={classes.showMore}>
         <div className={classes.info} onClick={onClick}><NavLink to={`/teams/${props.team.id}`} className={classes.info}>SHOW MORE</NavLink></div>
       </div>
